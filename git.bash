ea053de2d52ea97ab4a1492047f06d70b3ffc158
andytruong <andytruong@users.noreply.github.com>
2015-02-18 15:04:34 +0700

    Update and rename git/README.md to git.bash

---
+++ git.bash
@@ -0,0 +1,48 @@
+# Start
+# ---------------------
+git config --global user.name "First Last"
+git config --global user.email "username@domain.com"
+
+# Run git from other directory
+# ---------------------
+git --git-dir /path/to/.git/ push --all bb
+
+# pull and push all branches
+# ---------------------
+git push --all origin
+
+# Checkout
+# ---------------------
+# Checkout single branch
+git checkout --single-branch --branch=BranchName
+
+# Create new branch with no history
+git checkout --orphan branchName
+
+# Checkout a remote branch
+git remote update
+git fetch
+git checkout -b local-name origin/remote-name
+
+# History of directory/file
+# ---------------------
+git log --all --graph \
+  --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\n--abbrev-commit \
+  --date=relative
+
+# Release notes
+git log tag1...tag2 --pretty=oneline --reverse --abbrev-commit
+
+# Git submodule
+# ---------------------
+git submodule add https://github.com/doctrine/DoctrineBundle.git bundles/DoctrineBundle
+
+
+# Delete all tags
+# ---------------------
+git tag | xargs git tag -d 
+
+# Aliases
+# ---------------------
+ls = log --oneline
+caa = commit -a --amend -C HEAD
