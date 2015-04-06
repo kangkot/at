d090ab7ae062500b75c644da4bbf6e091819c833
Andy T. <andytruong@users.noreply.github.com>
2015-04-06 15:41:31 +0700

    Update sample.php

--- code/xhprof/sample.php
+++ code/xhprof/sample.php
@@ -1,15 +1,19 @@
 <?php
 
-xhprof_enable();
+if (!empty($_GET['profile'])) {
+  xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+  $XHPROF_ROOT = "/data/disk/o1/static/xhprof";
+  require_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
+  require_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
+}
 
-menu_execute_active_handler(); // The code
+define('DRUPAL_ROOT', getcwd());
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+menu_execute_active_handler();
 
-$xhprof_data = xhprof_disable();
-
-$XHPROF_ROOT = "/var/www/";
-include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
-include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
-
-$xhprof_runs = new XHProfRuns_Default();
-$run_id = $xhprof_runs->save_run($xhprof_data, $_SERVER['SERVER_NAME']);
-echo '<a href="http://example.com/index.php?run=', $run_id, '&source=', $_SERVER['SERVER_NAME'], '">XHProf #', $run_id, '</a>';
+if (!empty($_GET['profile'])) {
+  $xhprof_runs = new XHProfRuns_Default();
+  $run_id = $xhprof_runs->save_run($output = xhprof_disable(), $_SERVER['SERVER_NAME']);
+  echo '<a href="http://xhprof.dev.redfusionlabs.com/?run=', $run_id, '&source=', $_SERVER['SERVER_NAME'], '">XHProf #', $run_id, '</a>';
+}
